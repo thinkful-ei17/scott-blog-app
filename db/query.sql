5b3a883ea15407ccfbe70fd833a34009490691f1
A. Scott Tribble <scott.tribble@As-MacBook-Pro.local>
2018-01-04 13:55:11 -0800

    working through update

---
+++ db/query.sql
@@ -0,0 +1,53 @@
+CREATE TABLE stories (
+  id serial PRIMARY KEY,
+  title text NOT NULL,
+  content text
+);
+
+-- DELETE FROM stories;
+
+-- INSERT INTO stories
+--   (title, content) VALUES
+--     ('What the government doesn''t want you to know about cats', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+--     ),
+
+--     ('The most boring article about cats you''ll ever read', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+--     ),
+
+--     ('7 things lady gaga has in common with cats', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+--     ),
+
+--     ('The most incredible article about cats you''ll ever read', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+--     ),
+
+--     ('10 ways cats can help you live to 100', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
+
+--     ('9 reasons you can blame the recession on cats', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
+
+--     ('10 ways marketers are making you addicted to cats', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+--     ),
+
+--     ('11 ways investing in cats can make you a millionaire', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+--     ),
+
+--     ('Why you should forget everything you learned about cats', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+--    );
+
+SELECT title, id from stories;
+
+-- DROP TABLE stories;
+
+-- INSERT INTO restaurants
+--   (name, borough, cuisine) VALUES
+--     ('Rebecca''s', 'Queens', 'Comfort');
+
+-- CREATE TABLE customers(
+--   id serial PRIMARY KEY,
+--   modified timestamp DEFAULT current_timestamp,
+--   first_name text,
+--   last_name text NOT NULL
+-- );
+-- [ ] Create a `stories` table with the following 3 columns:
+--     - `id`: an auto-incrementing integer
+--     - `title`: regular text. Required.
+--     - `content`: regular text. Can be blank.
--- routers/stories-router.js
+++ routers/stories-router.js
@@ -2,55 +2,119 @@
 
 const express = require('express');
 const router = express.Router();
-
+// const knex = require('knex');
+// const pg = require('pg');
 var data = require('../db/dummy-data');
+const bodyParser = require('body-parser');
+const { DATABASE } = require('../config');
+const knex = require('knex')(DATABASE);
 
-// const { DATABASE } = require('../config');
-// const knex = require('knex')(DATABASE);
+router.use(bodyParser.json());
 
 /* ========== GET/READ ALL ITEMS ========== */
-router.get('/stories', (req, res) => {
-  if (req.query.search) {
-    const filtered = data.filter((obj) => obj.title.includes(req.query.search));
-    res.json(filtered);
-  } else {
-    res.json(data);
-  }
+router.get('/stories', (req, res, next) => {
+  knex.select('id', 'title', 'content')
+    .from('stories')
+    .then(results => res.json(results))
+    .catch(err => {
+      next(err);
+    });
+
 });
 
 /* ========== GET/READ SINGLE ITEMS ========== */
-router.get('/stories/:id', (req, res) => {
-  const id = Number(req.params.id);
-  const item = data.find((obj) => obj.id === id);
-  res.json(item);
+router.get('/stories/:id', (req, res, next) => {
+  knex.select('id', 'title', 'content')
+    .from('stories')
+    .where('id', req.params.id)
+    .then(function(results){
+      if(results.length === 0){
+        next();
+      }
+      else {
+        res.json(results[0]);
+      }
+    })  
+    .catch(next);
 });
 
 /* ========== POST/CREATE ITEM ========== */
-router.post('/stories', (req, res) => {
-  const {title, content} = req.body;
+router.post('/stories', (req, res, next) => {
   
-  /***** Never Trust Users! *****/  
-  const newItem = {
-    id: data.nextVal++,
-    title: title,
-    content: content
-  };
-  data.push(newItem);
+  const required = ['title', 'content'];
 
-  res.location(`${req.originalUrl}/${newItem.id}`).status(201).json(newItem);
+  required.forEach(requiredField => {
+    if (!(requiredField in req.body)) {
+      const errorMessage = `You're missing a required field: ${requiredField}`;
+      console.error(errorMessage);
+      res.status(400).end();
+      return;
+    }
+  });
+
+  knex('stories')
+    .insert({
+      title: req.body.title, 
+      content: req.body.content
+    })
+    .returning(['id', 'title', 'content']) 
+    .then(results => res.location(`/stories/${results.id}`).status(201).json(results[0]))
+    .catch(next);
 });
+
 
 /* ========== PUT/UPDATE A SINGLE ITEM ========== */
-router.put('/stories/:id', (req, res) => {
-  const {title, content} = req.body;
-  
-  /***** Never Trust Users! *****/
-  
-  const id = Number(req.params.id);
-  const item = data.find((obj) => obj.id === id);
-  Object.assign(item, {title, content});
-  res.json(item);
+router.put('/stories/:id', (req, res, next) => {
+
+  const required = ['title', 'content'];
+
+  required.forEach(requiredField => {
+    if (!(requiredField in req.body)) {
+      const errorMessage = `You're missing a required field: ${requiredField}`;
+      console.error(errorMessage);
+      res.status(400).end();
+      return;
+    }
+  });
+
+  knex('stories')
+    .where('id', req.params.id)
+    .update({
+      title: req.body.title,
+      content: req.body.content
+    })
+    .returning(['id', 'title', 'content'])
+    .then(function (results) {
+      if (results.length === 0) {
+        res.status(404).json(`The id ${req.params.id} doesn't exist`);
+      }
+      else {
+        res.json(results[0]);
+      }
+    })
+    .catch(next);
 });
+     
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+//   const {title, content} = req.body;
+  
+//   /***** Never Trust Users! *****/
+  
+//   const id = Number(req.params.id);
+//   const item = data.find((obj) => obj.id === id);
+//   Object.assign(item, {title, content});
+//   res.json(item);
+// });
 
 /* ========== DELETE/REMOVE A SINGLE ITEM ========== */
 router.delete('/stories/:id', (req, res) => {
